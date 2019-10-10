d487dc335626ff5d2a8d91ef89fd190bc9a5518e
Zachary <pagezacharyc@gmail.com>
2019-10-10 18:30:53 -0500

    Updated Gallery Skeleton 10-10-19

---
+++ src/gallery.js
@@ -0,0 +1,11 @@
+import zach from './assets/zach_p.jpeg'
+
+const gallery = [
+  {
+    id: 1,
+    image: zach,
+    alt: 'Historian'
+  }
+];
+
+export default gallery;
--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './gallery-grid.css';
+import gallery from '../../gallery';
 import { Card, Col, Container, Row } from 'react-bootstrap';
+
 
 class Gallery extends React.Component {
   render() {
@@ -12,9 +14,22 @@ class Gallery extends React.Component {
         <div class='tz-gallery'>
             <div class="row">
                 <div class="col-sm-6 col-md-4">
-                    <a class="lightbox" href="././assets/gallery/1.jpg">
-                        <img src="./1.jpg" alt='1'></img>
-                    </a>
+                    <Container
+                        fluid
+                        className='galleryCard'
+                        style={{ marginBottom: '3em' }}
+                        >
+                        <Row>
+                            <div className='galleryImages'>
+                            {gallery.map((gallery) => (
+                                <Card key={gallery.id}>
+                                <Card.Link href={gallery.image}>Download Here</Card.Link>
+                                <Card.Img variant='top' src={gallery.image} />
+                                </Card>
+                            ))}
+                            </div>
+                        </Row>
+                    </Container>
                 </div>
             </div>
         </div>
